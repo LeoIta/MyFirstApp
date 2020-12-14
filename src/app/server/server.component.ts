2fc1609fc5534fda78b222e5bf2c284192ab22a1
LeoIta <leonardolorusso83@gmail.com>
2020-12-14 10:21:06 +0100

    ngStyle ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,28 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color: white;
+    }
+    `]
 })
 export class ServerComponent{
     serverName :string = 'Global Server';
     serverId : number = 10;
     serverStatus :string = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random()>0.5 ? 'online':'offline';
+    }
     getServerName(){
         return this.serverName;
+    }
+    getServerStatus(){
+        return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus === 'offline' ? 'red':'green'; 
     }
 }
